02a9bffa0d30f55de8ebdabc2550821024ba6131
DonizeteVida <donizetevida1998@outlook.com>
2026-03-10 01:29:11 +0100

    Reduce SettingsScreen.kt preview

--- app/src/main/java/com/dv/apps/komic/reader/feature/settings/SettingsScreen.kt
+++ app/src/main/java/com/dv/apps/komic/reader/feature/settings/SettingsScreen.kt
@@ -12,7 +12,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.PreviewScreenSizes
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dv.apps.komic.reader.feature.settings.folder.FolderSourceSettingsSection
 import com.dv.apps.komic.reader.feature.settings.preview.PreviewSettingsSection
@@ -49,7 +49,7 @@ fun SettingsSection(
     }
 }
 
-@PreviewScreenSizes
+@Preview
 @Composable
 private fun SettingsScreenPreview() {
     KomicReaderTheme {
