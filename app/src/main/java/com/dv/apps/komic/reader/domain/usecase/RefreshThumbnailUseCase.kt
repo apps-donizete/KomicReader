e1cbd914a483f9e18a86400a8203f9488d1d8784
DonizeteVida <donizetevida1998@outlook.com>
2026-03-10 03:56:29 +0100

    Speed up thumbnail generation

--- app/src/main/java/com/dv/apps/komic/reader/domain/usecase/RefreshThumbnailUseCase.kt
+++ app/src/main/java/com/dv/apps/komic/reader/domain/usecase/RefreshThumbnailUseCase.kt
@@ -1,41 +1,35 @@
 package com.dv.apps.komic.reader.domain.usecase
 
-import com.dv.apps.komic.reader.domain.model.Settings
 import com.dv.apps.komic.reader.domain.repository.SettingsManager
 import com.dv.apps.komic.reader.domain.repository.ThumbnailManager
 import com.dv.apps.komic.reader.filesystem.platform.PlatformFile
 import com.dv.apps.komic.reader.filesystem.platform.PlatformFileManager
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.util.LinkedList
 
 class RefreshThumbnailUseCase(
     private val settingsManager: SettingsManager,
     private val platformFileManager: PlatformFileManager,
     private val thumbnailManager: ThumbnailManager
 ) : suspend () -> Unit {
-    override suspend fun invoke() = withContext(Dispatchers.IO) {
+    override suspend fun invoke() {
         val settings = settingsManager.getSettings().first()
         val quality = settings.quality
-        val selectedFolders = settings.selectedFolders
+        val platformFiles = settings.selectedFolders.mapNotNull(platformFileManager::get)
+        val linkedList = LinkedList(platformFiles)
 
-        for (selectedFolder in selectedFolders) {
-            val platformFile = platformFileManager.get(selectedFolder) ?: continue
-            generateThumbnail(platformFile, quality)
-        }
-    }
-
-    //TODO(improve async here)
-    private suspend fun generateThumbnail(
-        platformFile: PlatformFile,
-        quality: Settings.Quality
-    ) {
-        if (platformFile.type is PlatformFile.Type.File) {
-            thumbnailManager.generate(platformFile, quality)
-            return
-        }
-        for (platformFile in platformFileManager.listFiles(platformFile)) {
-            generateThumbnail(platformFile, quality)
+        withContext(Dispatchers.IO) {
+            while (linkedList.isNotEmpty()) {
+                val platformFile = linkedList.pop()
+                if (platformFile.type is PlatformFile.Type.Folder) {
+                    linkedList.addAll(platformFileManager.listFiles(platformFile))
+                    continue
+                }
+                launch { thumbnailManager.generate(platformFile, quality) }
+            }
         }
     }
 }
